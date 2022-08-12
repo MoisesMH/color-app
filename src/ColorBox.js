c0b13beb1431d15631c44f59db8249fe48b3739f
Moises Melendez Hualpa <moise.melendez@hotmail.com>
2022-08-12 11:41:06 -0500

    Implemented copy-to-clipboard

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,28 +1,35 @@
 import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import Button from "./Button";
 import "./ColorBox.css"
 
 class ColorBox extends Component {
     constructor(props) {
         super(props)
-        this.copyToClipboard = this.copyToClipboard.bind(this)
+        // this.copyToClipboard = this.copyToClipboard.bind(this)
+        this.state = { copied: false }
     }
 
-    copyToClipboard() {
-        console.log(this.props.color)
-    }
+    // Copy to clipboard with a single command
+    // async copyToClipboard() {
+    //     await navigator.clipboard.writeText(this.props.color)
+    // }
+    // <div className="ColorBox" style={{ backgroundColor: this.props.color }} onClick={this.copyToClipboard}><div>
+    // Another way to copy to clipboard is using the library react-copy-to-clipboard
 
     render() {
         return (
-            <div className="ColorBox" style={{ backgroundColor: this.props.color }}>
-                <div className="copy-container">
-                    <div className="box-content">
-                        <span>{this.props.name}</span>
+            <CopyToClipboard text={this.props.color}>
+                <div className="ColorBox" style={{ backgroundColor: this.props.color }}>
+                    <div className="copy-container">
+                        <div className="box-content">
+                            <span>{this.props.name}</span>
+                        </div>
+                        <Button className="copy-button" text="Copy" />
                     </div>
-                    <Button className="copy-button" func={this.copyToClipboard} text="Copy" />
+                    <span className="see-more">More</span>
                 </div>
-                <span className="see-more">More</span>
-            </div>
+            </CopyToClipboard>
         )
     }
 }
