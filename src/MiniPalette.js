ddd905125a064652415ab9345a9a63f67cb3d1e6
Moises Melendez Hualpa <moise.melendez@hotmail.com>
2022-08-15 04:24:04 -0500

    Added MiniPalette and its styles
    Implemented the miniPalette Component using styled library from mui/material, an alternative to old css-to-js (jss) method.

---
+++ src/MiniPalette.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { styled } from "@mui/material";
+
+const PREFIX = 'MiniPalette';
+const classes = {
+    main: `${PREFIX}`,
+    colors: `${PREFIX}-colors`,
+    title: `${PREFIX}-title`,
+    emoji: `${PREFIX}-emoji`,
+    miniColor: `${PREFIX}-miniColor`
+}
+
+const Main = styled('div')({
+    [`&.${classes.main}`]: {
+        backgroundColor: "white",
+        border: "1px solid black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        position: "relative",
+        overflow: "hidden",
+        "&:hover": {
+            cursor: "pointer"
+        }
+    },
+    [`& .${classes.colors}`]: {
+        backgroundColor: "#dae1e4",
+        // for the parent
+        height: "150px",
+        width: "100%",
+        borderRadius: "5px",
+        // A workaround to overcome the dispaired edges problem
+        overflow: "hidden"
+    },
+    [`& .${classes.title}`]: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "0",
+        color: "black",
+        paddingTop: "0.5rem",
+        fontSize: "1rem",
+        position: "relative"
+    },
+    [`& .${classes.emoji}`]: {
+        marginLeft: "0.5rem",
+        fontSize: "1.5rem"
+    },
+    [`& .${classes.miniColor}`]: {
+        height: "25%",
+        width: "20%",
+        display: "inline-block",
+        margin: "0 auto",
+        position: "relative",
+        marginBottom: "-3.5px"
+    }
+})
+
+function MiniPalette({ paletteName, emoji, colors }){
+    const miniColorBoxes = colors.map(color => (
+        <div 
+            className={classes.miniColor}
+            style={{ backgroundColor: color.color }}
+            key={color.name}
+        />
+    ))
+    return (
+        <Main className={classes.main}>
+            <div className={classes.colors}>{ miniColorBoxes }</div>
+            <h5 className={classes.title}>
+                {paletteName} <span className={classes.emoji}>{ emoji }</span>
+            </h5>
+        </Main>
+    )
+}
+
+export default MiniPalette
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,15 +1,68 @@
 import React from "react"
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
+import MiniPalette from "./MiniPalette"
+import { styled } from "@mui/material"
+
+const PREFIX = 'PaletteList';
+const classes = {
+    root: `${PREFIX}`,
+    container: `${PREFIX}-container`,
+    nav: `${PREFIX}-nav`,
+    palettes: `${PREFIX}-palettes`
+}
+
+const Root = styled('div')({
+    [`&.${classes.root}`]: {
+        backgroundColor: "blue",
+        height: "100vh",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center"
+    },
+    [`& .${classes.container}`]: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap"
+    },
+    [`& .${classes.nav}`]: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-betweem",
+        color: "white"
+    },
+    [`& .${classes.palettes}`]: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        // This created three columns with two gaps
+        gridTemplateColumns: "repeat(3, 30%)",
+        // Its space between the columns is 5% to complete the 100% space width
+        gridGap: "5%"
+    }
+})
 
 function PaletteList({ palettes }) {
-    const pals = palettes.map(pal => (
-        <p><Link to={`/palette/${pal.id}`}>{pal.paletteName}</Link></p>
+    // const pals = palettes.map(pal => (
+    //     <p><Link to={`/palette/${pal.id}`}>{pal.paletteName}</Link></p>
+    // ))
+
+    const minipals = palettes.map(pal => (
+        <MiniPalette {...pal} />
     ))
+
     return (
-        <div className="PaletteList">
-            <h1>React Colors</h1>
-            { pals }
-        </div>
+        <Root className="PaletteList">
+            <div className="PaletteList-container">
+                <nav className="PaletteList-nav">
+                    <h1>React Colors</h1>
+                </nav>
+                <div className="PaletteList-palettes">
+                    {minipals}
+                </div>
+            </div>
+        </Root>
     )
 }
 
